cc553b2ee00dd1cf0c8af6fa7c4b5468ca264fc4
VitaminD99 <dung200899@gmail.com>
2023-05-18 11:48:12 +0700

    late files

---
+++ conditionals.php
@@ -0,0 +1,11 @@
+<?php
+    // echo "We talk about conditional in PHP<br>";
+    $age = 15;
+    // if($age >= 18){
+    //     echo "You...";
+    // } else{
+    //     echo "Y..";
+    // }
+    $hours = date("H");
+    echo $hours 
+?>
---
+++ variables.php
@@ -0,0 +1,16 @@
+<?php
+    echo "hello ";
+    $name = 'Dung';
+    $age = '24';
+    $has_mercedes = false;
+    $product_price = 22.45;
+    echo '<br>';
+    // echo "${name} is ${age} years old";
+    echo '1' +'2';
+    $sum = '2' +'3';
+    // var_dump($sum);
+    // constants
+    define('SERVER_NAME', 'localhost');
+    define('DATEBASE_NAME', 'test_db');
+    echo "server: ".SERVER_NAME.' ,db : ' .DATEBASE_NAME;
+?>
