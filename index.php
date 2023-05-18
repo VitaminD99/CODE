e00d5ed3c15e0346a27fff31bf3a8d83d3d87083
VitaminD99 <dung200899@gmail.com>
2023-05-18 11:46:36 +0700

    latest files

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    // echo "pi = ",3,14, ', x = ', 3;
+    // echo ['john','Hoang']
+    // print "hello";
+    // print_r(['john','Hoang']);
+    // var_dump(false);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?php echo 'Hallo';?></h1>
+</body>
+</html>
